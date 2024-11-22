9a2f7d2e4227bd9865e548e7de83e1b0cfad4263
Letícia Zanichelli de Oliveira <zanilizchelli@gmail.com>
2024-11-22 00:43:38 -0300

    Add files via upload

---
+++ flashcard/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
+
+criaCartao(
+    'Esporte',
+    'Qual a melhor Manobra de Skate?',
+    'Ollie, Criado em 1987, pelo lendário skatista americano Alan “Ollie” Gelfand, ele consiste em saltar com o skate sem usar as mãos, arrastando o pé até o nose'
+)
+
+criaCartao(
+    'Filme/Livro',
+    'Qual o Patrono do Harry Potter?',
+    'Tem a forma de um cervo'
+    )
+
+criaCartao(
+    'Astronomia',
+    'Qual o Planeta mais quente?',
+    'Vênus, Apesar de não ser próximo do Sol, é o planeta mais quente do nosso sistema e chega á temperaturas ficam sempre acima dos 400 °C.'
+)
